4d99d723dd651ca63bcfad1eba474b8e3841bcb3
EslamHamdy911 <ee5139901@gmail.com>
2025-11-09 04:57:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+let currentGame = null;
 
+function loadGame(name) {
+  if (currentGame && currentGame.stop) currentGame.stop();
+  if (name === "snake") currentGame = SnakeGame();
+  if (name === "pong") currentGame = PongGame();
+}
